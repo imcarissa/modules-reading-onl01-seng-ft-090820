21579edf59a3c80d5b196e3d66aa59cf7e866599
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-27 06:45:31 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -6,8 +6,7 @@ require_relative './fancy_dance.rb'
 class Kid 
   include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
-  
-  
+
   attr_accessor :name 
   
   def initialize(name)
